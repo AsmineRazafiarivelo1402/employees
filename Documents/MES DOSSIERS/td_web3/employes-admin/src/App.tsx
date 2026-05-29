a35215a012cf643b164aa54d209f3389d3cea3b6
Miriasme <johndoe@example.com>
2026-05-29 21:51:13 +0300

    feat: add 2)Employee List

--- Documents/MES DOSSIERS/td_web3/employes-admin/src/App.tsx
+++ Documents/MES DOSSIERS/td_web3/employes-admin/src/App.tsx
@@ -1,8 +1,9 @@
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
 import { dataProvider } from './dataProvider';
+import { EmployeeList } from './employees/EmployeeList';
 
 export const App = () => (
     <Admin dataProvider={dataProvider}>
-<Resource name="employes" list={ListGuesser} />
+<Resource name="employees" list={EmployeeList} />
     </Admin>
 );
